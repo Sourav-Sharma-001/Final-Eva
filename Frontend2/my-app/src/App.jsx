e31047ae31b68a3a3c63fcc99706db3de80e6c88
WatchOut-RPG <sourav060606@gmail.com>
2025-10-26 18:31:38 +0530

    menu component created

--- Frontend2/my-app/src/App.jsx
+++ Frontend2/my-app/src/App.jsx
@@ -1,10 +1,11 @@
+import React from 'react'             // <--- add this
 import './App.css'
+import Menu from "./Components/Menu/Menu"  // import default export
 
 function App() {
-
   return (
     <>
-      
+      <Menu />
     </>
   )
 }
